4f6350fb7381da7abbb62bd8fac55b1ec783ca8e
carlosbr86 <carlosbr86@gmail.com>
2017-05-23 16:02:15 -0400

    matt request

--- react-rest-front/src/App.js
+++ react-rest-front/src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import $ from "jquery";
+import 'react-datepicker/dist/react-datepicker.css';
+
+
 // import './App.css';
 // import {Link} from 'react-router';
 
 
-class ProgressArc extends Component {
+
+class D3Component extends Component { 
   
-
-
-
-  componentDidMount() {
-    const context = this.setContext();
-    this.setLine(context);
-    this.setXAxis(context);
-    this.setYAxis(context);
-  }
+  
   setContext() {
-    let svg = d3.select(this.refs.arc) .append("svg")
-        .attr("width", '700px') //width + margin.left + margin.right
+    let svg=d3.select(this.refs.arc).append("svg")
+.attr("width", '700px') //width + margin.left + margin.right
         .attr("height", '400px') //height + margin.top + margin.bottom
     .append("g")
         .attr("transform",
@@ -28,21 +27,20 @@ class ProgressArc extends Component {
 
   setLine(context) {
     
-    var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
-    width = 600 - margin.left - margin.right,            //600 hardCode Data
-    height = 270 - margin.top - margin.bottom;           //270 hardCode Data
-    console.log("d3=>",d3);
-  var parseTime = d3.time.format("%Y-%m-%d").parse;       // Changed parseDate to parseTime
+  var margin={top: 30, right: 20, bottom: 30, left: 50}, 
+    width=600 - margin.left - margin.right,            //600 hardCode Data
+    height=270 - margin.top - margin.bottom;           //270 hardCode Data
+  var parseTime=d3.time.format("%Y-%m-%d").parse;       // Changed parseDate to parseTime
   // Set the ranges
-  var x = d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
-  var y = d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
+  var x=d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
+  var y=d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
   // Define the axis
-  var xAxis = d3.svg.axis().scale(x) .orient("bottom").ticks(5);
-  var yAxis = d3.svg.axis().scale(y) .orient("left").ticks(5);
+  var xAxis=d3.svg.axis().scale(x).orient("bottom").ticks(5);
+  var yAxis=d3.svg.axis().scale(y).orient("left").ticks(5);
   // Define the line
-  var valueline = d3.svg.line()                             
-  .x(function(d) { return x(d.date); }) 
-  .y(function(d) { return y(d.value); });
+  var valueline=d3.svg.line()                             
+    .x(function(d) { return x(d.date); }) 
+    .y(function(d) { return y(d.value); });
 
   // Get the data
   d3.csv("data.csv", function(error, data) { 
@@ -68,21 +66,21 @@ class ProgressArc extends Component {
           });
   }
 
-setXAxis(context) {
+  setXAxis(context) {
     
-    var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
-    width = 600 - margin.left - margin.right,            //600 hardCode Data
-    height = 270 - margin.top - margin.bottom;           //270 hardCode Data
-    console.log("d3=>",d3);
+  var margin={top: 30, right: 20, bottom: 30, left: 50}, 
+    width=600 - margin.left - margin.right,            //600 hardCode Data
+    height=270 - margin.top - margin.bottom;           //270 hardCode Data
+
   var parseTime = d3.time.format("%Y-%m-%d").parse;       // Changed parseDate to parseTime
   // Set the ranges
-  var x = d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
-  var y = d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
+  var x=d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
+  var y=d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
   // Define the axis
-  var xAxis = d3.svg.axis().scale(x) .orient("bottom").ticks(5);
-  var yAxis = d3.svg.axis().scale(y) .orient("left").ticks(5);
+  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
   // Define the line
-  var valueline = d3.svg.line()                             
+  var valueline= d3.svg.line()                             
   .x(function(d) { return x(d.date); }) 
   .y(function(d) { return y(d.value); });
 
@@ -105,29 +103,28 @@ setXAxis(context) {
           });
   }
 
-setYAxis(context) {
+  setYAxis(context) {
     
-    var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
+  var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
     width = 600 - margin.left - margin.right,            //600 hardCode Data
     height = 270 - margin.top - margin.bottom;           //270 hardCode Data
-    console.log("d3=>",d3);
   var parseTime = d3.time.format("%Y-%m-%d").parse;       // Changed parseDate to parseTime
   // Set the ranges
-  var x = d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
-  var y = d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
+  var x=d3.time.scale().rangeRound([0, width]);         // Changed range. to rangeRound            - older code , only d3 instead d3.time
+  var y=d3.scale.linear().rangeRound([height, 0]);      // Changed range. to rangeRound
   // Define the axis
-  var xAxis = d3.svg.axis().scale(x) .orient("bottom").ticks(5);
-  var yAxis = d3.svg.axis().scale(y) .orient("left").ticks(5);
+  var xAxis=d3.svg.axis().scale(x).orient("bottom").ticks(5);
+  var yAxis=d3.svg.axis().scale(y).orient("left").ticks(5);
   // Define the line
-  var valueline = d3.svg.line()                             
+  var valueline=d3.svg.line()                             
   .x(function(d) { return x(d.date); }) 
   .y(function(d) { return y(d.value); });
 
   // Get the data
   d3.csv("data.csv", function(error, data) { 
       data.forEach(function(d) {
-          d.date = parseTime(d.date);
-          d.value = +d.value;
+          d.date=parseTime(d.date);
+          d.value=+d.value;
       });
       // Scale the range of the data
 
@@ -137,8 +134,8 @@ setYAxis(context) {
 
     context.append("g")
         .attr("class", "y axis")
-        .call(yAxis).
-        append("text")        // all attributes bellow are NEW 
+        .call(yAxis)
+        .append("text")        // all attributes bellow are NEW 
       .attr("fill", "#000") // 
       .attr("transform", "rotate(-90)")  // 
       .attr("y", 6) // 
@@ -148,8 +145,19 @@ setYAxis(context) {
           });
   }
 
+  componentWillMount() {
+    console.log()
+  }
+
+  componentDidMount() { // Component Did Update?
+    const context = this.setContext();
+    this.setLine(context);
+    this.setXAxis(context);
+    this.setYAxis(context);
+  }
+
   render() {
-var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
+  var margin = {top: 30, right: 20, bottom: 30, left: 50}, 
     width = 600 - margin.left - margin.right,            //600 hardCode Data
     height = 270 - margin.top - margin.bottom;           //270 hardCode Data
 
@@ -162,169 +170,6 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
     )
   }
 }
-
-
-/*               LINE CHART EXAMPLE
-const Line = React.createClass({
-
-  propTypes: {
-    path:         React.PropTypes.string.isRequired,
-    stroke:       React.PropTypes.string,
-    fill:         React.PropTypes.string,
-    strokeWidth:  React.PropTypes.number
-  },
-
-  getDefaultProps() {
-    return {
-      stroke:       'blue',
-      fill:         'none',
-      strokeWidth:  3
-    };
-  },
-
-  render() {
-    let { path, stroke, fill, strokeWidth } = this.props;
-    return (
-      <path d={path} fill={fill} stroke={stroke} strokeWidth={strokeWidth} />
-    );
-  }
-
-});
-
-
-
-const DataSeries = React.createClass({
-
-  propTypes: {
-    colors:             React.PropTypes.func,
-    data:               React.PropTypes.object,
-    interpolationType:  React.PropTypes.string,
-    xScale:             React.PropTypes.func,
-    yScale:             React.PropTypes.func
-  },
-
-  getDefaultProps() {
-    return {
-      data:               [],
-      interpolationType:  'cardinal',
-      // colors:             d3.scale.category10()
-    };
-  },
-
-  render() {
-    let { data, colors, xScale, yScale, interpolationType } = this.props;
-
-    let line = d3.svg.line()
-      .interpolate(interpolationType)
-      .x((d) => { return xScale(d.x); })
-      .y((d) => { return yScale(d.y); });
-
-    let lines = data.points.map((series, id) => {
-      return (
-        <Line
-          path={line(series)}
-          stroke={colors(id)}
-          key={id}
-          />
-      );
-    });
-
-    return (
-      <g>
-        <g>{lines}</g>
-      </g>
-    );
-  }
-
-});
-
-const LineChart = React.createClass({
-
-  propTypes: {
-    width:  React.PropTypes.number,
-    height: React.PropTypes.number,
-    data:   React.PropTypes.object.isRequired
-  },
-
-  getDefaultProps(){
-    return {
-      width:  600,
-      height: 300
-    }
-  },
-
-  render() {
-    let { width, height, data } = this.props;
-
-   let xScale =0 ;    // d3.scale.ordinal()
-    //                .domain(data.xValues)
-    //                .rangePoints([0, width]);
-
-       let yScale =0 // d3.scale.linear()
-    //                .range([height, 10])
-    //                .domain([data.yMin, data.yMax]);
-
-    return (
-      <svg width={width} height={height}>
-          <DataSeries
-            xScale={xScale}
-            yScale={yScale}
-            data={data}
-            width={width}
-            height={height}
-            />
-      </svg>
-    );
-  }
-
-});
-
-*/
-//################################################### APP ####################################################
-
-class App extends Component {
-  constructor (){
-    super ();
-
-  }
-  render() {
-    let data = {
-    points: [
-      [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 },
-        { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
-      ,
-      [ { x: 0, y: 8 }, { x: 1, y: 5 }, { x: 2, y: 20 }, { x: 3, y: 12 },
-        { x: 4, y: 4 }, { x: 5, y: 6 }, { x: 6, y: 2 } ]
-      ,
-      [ { x: 0, y: 0 }, { x: 1, y: 5 }, { x: 2, y: 8 }, { x: 3, y: 2 },
-        { x: 4, y: 6 }, { x: 5, y: 4 }, { x: 6, y: 2 } ]
-      ],
-    xValues: [0,1,2,3,4,5,6],
-    yMin: 0,
-    yMax: 30
-  };
-
-
-    return (
-      <div className="App">
-        {/*{this.props.children} */}
-        <h2>Currency Graph</h2>
-        {/*<D3/>*/}
-        <ProgressArc height={300} width={300} innerRadius={100} outerRadius={110} id="d3-arc" backgroundColor="#e6e6e6" foregroundColor="#00ff00"
-            percentComplete={0.3} />
-        {/*<LineChart data={data} width={600} height={300} />,*/}
-
-
-        <Form/>
-      </div>
-    );
-  };
-};
-
-console.log('d3', d3)
-
-export default App;
-//################################################### APP ####################################################
 
 class Form extends Component {
   sendform(){
@@ -364,8 +209,9 @@ class Form extends Component {
   render(){
     return(
       <div>
-        <p>Start Date: <input type="text" id="datepickerStart" value="2017-04-30"/></p>
-        <p> Final Date: <input type="text" id="datepickerFinal" value="2017-05-20"/></p>
+        <p>Start Date: <input type="text" id="datepickerStart" /></p> {/*value="2017-04-30"*/}
+        <p> Final Date: <input type="text" id="datepickerFinal" /></p>{/*value="2017-05-20"*/}
+
         <p> Base Currency:</p>
         <select id ="baseCurrency" name="baseCurrency">
             <option value="AUD"> AUD - Australian Dollar </option>
@@ -435,8 +281,80 @@ class Form extends Component {
             <option value="ZAR">ZAR - South African Rand </option>
     </select>
     <p></p>
-      <button id="submitForm"> Submit</button>
+      <button id="submitForm" onClick={this.props.handleSubmit} > Submit</button>
     </div>
     )
   }
 }
+
+// class Calendar extends Component {
+//   constructor (props) {
+//     super (props)
+//     this.state= {
+//       startDate: moment ()
+//     };
+//       this.handleChange = this.handleChange.bind(this);
+
+//   }
+
+//   handleChange(date) {
+//     this.setState({
+//       startDate: date
+//     });
+//   }
+//   render () {
+//     return <DatePicker selected={this.state.startDate} onChange= {this.handleChange}/>
+//   }  
+// }
+
+
+
+class App extends Component {
+  constructor (props){
+    super (props);
+    this.state = {
+      startDate: "2017-04-30",
+      finalDate: "2017-05-20",
+      baseCurrency: "CAD",
+      symbolCurrency: "BRL"
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(){
+    console.log("datepickerStart=>",document.getElementById("datepickerStart").value);
+    // startDate = $("input[id = 'datepickerStart']").val();
+    // finalDate = $("input[id = 'datepickerFinal']").val();
+    // baseCurrency = $("select[id = 'baseCurrency']").val();
+    // symbolCurrency = $("select[id = 'symbolCurrency']").val();
+  }
+
+    componentDidMount() {
+      // console.log($.datepicker);
+      //     $(document).ready(function () {
+      //       $(function () {
+      //           $("#datepickerStart").datepicker({
+      //               dateFormat: "yy-mm-dd"
+      //           });
+      //       });
+      //       $(function () {
+      //           $("#datepickerFinal").datepicker({
+      //               dateFormat: "yy-mm-dd"
+      //           });
+      //       });
+      //     });
+  }
+  render() {
+    return (
+      <div className="App">
+        {/*{this.props.children} */}
+        <h2>Currency Graph</h2>
+        <D3Component />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
+  };
+};
+
+
+export default App;
+//################################################### APP ####################################################
